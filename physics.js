175ddd0e7f616a3fa109fd5c25cdddf4cc542d6d
Mees Brinkhuis <M.Brinkhuis@student.tudelft.nl>
2021-04-04 18:12:44 +0200

    simplified wall bounce

--- physics.js
+++ physics.js
@@ -1,7 +1,7 @@
-const fr = 300
+const fr = 3000
 const px_m = 5
-let g = 5
-const bounce = .75
+let g = 9.81
+const bounce = .8
 
 g_acc = 0
 
@@ -28,33 +28,70 @@ let obj = [
 
 const obst = [
   {
-    x0: 100,
-    y0: 800,
-    x1: 400,
-    y1: 700 
+    x0: 50,
+    y0: 0,
+    x1: 0,
+    y1: 1000 
   },
   {
-    x0: 50,
-    y0: 600,
-    x1: 75,
-    y1: 900 
-  }
+    x0: 0,
+    y0: 20,
+    x1: 500,
+    y1: 0 
+  },
+  {
+    x0: 500,
+    y0: 0,
+    x1: 450,
+    y1: 1000 
+  },
+  {
+    x0: 0,
+    y0: 1000,
+    x1: 500,
+    y1: 980 
+  },
 ]
 
 function hit_obst(ob){
   for(let o of obst){
-    let [distance, rdx, rdy] = reflect_angle(ob, o)
+    
+    let distance = line_point_distance(ob, o)
     if(distance > ob.size/2) continue
-    
     let overlap = ob.size/2 - distance
-    let speed = Math.sqrt(ob.dx*ob.dx + ob.dy*ob.dy)
     
-    ob.dx = rdx*speed*bounce
-    ob.dy = rdy*speed*bounce
+    let [nx, ny] = normalize_vector(-(o.y0 - o.y1), o.x0 - o.x1)
+            
+    let dot = ob.dx*nx + ob.dy*ny
+    let vx = -ob.dx + 2*(dot)*nx
+    let vy = -ob.dy + 2*(dot)*ny
+
+    ob.dx = -vx*bounce
+    ob.dy = -vy*bounce
     
-    ob.x += rdx*overlap
-    ob.y += rdy*overlap
+    let [dx, dy] = normalize_vector(vx, vy)
+    
+    ob.x += -dx*overlap
+    ob.y += -dy*overlap
   }
+}
+
+function line_point_distance(ob, obs){
+  la = -(obs.y1 - obs.y0)/(obs.x1 - obs.x0)
+  lb = -(obs.y0 + (la * obs.x0))
+  
+  // right angle line
+  let [dx, dy] = normalize_vector(-(obs.y0 - obs.y1), obs.x0 - obs.x1)
+    
+  // right angle equation
+  pa = -dy/dx
+  pb = -(ob.y + (pa * ob.x))  
+  
+  // get intersection point right angle
+  ix = (lb-pb)/(pa-la)
+  iy = -(pa*ix+pb)
+  
+  return Math.abs(Math.sqrt((ob.x - ix)*(ob.x - ix) + (ob.y - iy)*(ob.y - iy)))
 }
 
 function keyPressed() {
@@ -75,10 +112,10 @@ function hit_obj(ob){
     if(ob == ext_ob) continue
     
     let distance = Math.sqrt((ext_ob.x - ob.x)**2 + (ext_ob.y - ob.y)**2)
-    if (distance > ob.size) continue
+    if (distance > (ob.size/2)+(ext_ob.size/2)) continue
     let overlap = ob.size - distance
     
-    let [dx, dy] = normalize_delta((ob.x - ext_ob.x), (ob.y - ext_ob.y))
+    let [dx, dy] = normalize_vector((ob.x - ext_ob.x), (ob.y - ext_ob.y))
     
     let vx = Math.abs(ob.dx - ext_ob.dx)
     let vy = Math.abs(ob.dy - ext_ob.dy)
@@ -112,64 +149,9 @@ function draw_obst(){
   }
 }
   
-function normalize_delta(dx, dy){
+function normalize_vector(dx, dy){
   unit = Math.sqrt((dx)*(dx) + (dy)*(dy))
   return [dx/unit, dy/unit]
-}
-
-function reflect_angle(ob, obs){
-  
-  // equation for the line 
-  // get formula for the line
-  la = -(obs.y1 - obs.y0)/(obs.x1 - obs.x0)
-  lb = -(obs.y0 + (la * obs.x0))
-  
-  // right angle line
-  let [dx, dy] = normalize_delta(-(obs.y0 - obs.y1), obs.x0 - obs.x1)
-    
-  // right angle equation
-  pa = -dy/dx
-  pb = -(ob.y + (pa * ob.x))  
-  
-  // get intersection point right angle
-  ix = (lb-pb)/(pa-la)
-  iy = -(pa*ix+pb)
-    
-  // get formula attack line
-  pda = -ob.dy/ob.dx
-  pdb = -(ob.y + (pda * ob.x))  
-  
-  // get intersection point reflection
-  rx = (lb-pdb)/(pda-la)
-  ry = -(pda*rx+pdb)
-    
-  // virtual point
-  vx = ob.x - (ob.x-ix)*2
-  vy = ob.y - (ob.y-iy)*2
-  
-  // calc unit delta for reflection
-  let [udx, udy] = normalize_delta(rx - vx, ry - vy)
-  
-  // debug
-//   line(rx, ry, rx+(udx*200), ry+(udy*200))
-    
-//   fill('#00FF00')
-//   circle(vx, vy, 10)
-    
-//   fill('#FFFF00')
-//   circle(rx, ry, 10)
-//   line(ob.x, ob.y, rx, ry)
-    
-//   fill('#FF0000')
-//   circle(ix, iy, 10)
-//   line(ob.x, ob.y, ix, iy)
-  
-  // get distance and reflection unit vector
-  let distance = Math.sqrt((ob.x - ix)*(ob.x - ix) + (ob.y - iy)*(ob.y - iy))
-  if(isNaN(distance)) distance = Infinity
-  if(isNaN(udx)) udx = 0
-  if(isNaN(udy)) udy = 0
-  return [distance, udx, udy]
 }
 
 function apply_f(ob){
@@ -183,7 +165,7 @@ function mouseClicked() {
   obj.push(
     {
       id: obj.length,
-      size: 30,
+      size: 20,
       x: mouseX,
       y: mouseY,
       dx: 0.0001,
