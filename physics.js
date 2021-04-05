43491b0761f4fd73db15adb8e234553fd002cef5
Mees Brinkhuis <M.Brinkhuis@student.tudelft.nl>
2021-04-05 13:53:11 +0200

    made obst interaction  more like ob interaction

--- physics.js
+++ physics.js
@@ -1,6 +1,6 @@
-const fr = 3000
+const fr = 120
 const px_m = 5
-let g = 9.81
+let g = 3
 const bounce = .8
 
 g_acc = 0
@@ -8,7 +8,7 @@ g_acc = 0
 let obj = [
   {
     id: 0,
-    size: 30,
+    size: 25,
     x: 300,
     y: 250,
     dx: 0,
@@ -17,7 +17,7 @@ let obj = [
   },
   {
     id: 1,
-    size: 30,
+    size: 25,
     x: 279,
     y: 300,
     dx: 0,
@@ -34,16 +34,16 @@ const obst = [
     y1: 1000 
   },
   {
-    x0: 0,
-    y0: 20,
-    x1: 500,
-    y1: 0 
-  },
-  {
     x0: 500,
     y0: 0,
-    x1: 450,
-    y1: 1000 
+    x1: 0,
+    y1: 20 
+  },
+  {
+    x0: 450,
+    y0: 1000,
+    x1: 500,
+    y1: 0 
   },
   {
     x0: 0,
@@ -57,41 +57,54 @@ function hit_obst(ob){
   for(let o of obst){
     
     let distance = line_point_distance(ob, o)
-    if(distance > ob.size/2) continue
-    let overlap = ob.size/2 - distance
+    if(Math.abs(distance) > ob.size/2) continue
+    let overlap = ob.size/2 - Math.abs(distance)
     
+    // get normal vector
     let [nx, ny] = normalize_vector(-(o.y0 - o.y1), o.x0 - o.x1)
-            
-    let dot = ob.dx*nx + ob.dy*ny
-    let vx = -ob.dx + 2*(dot)*nx
-    let vy = -ob.dy + 2*(dot)*ny
+    
+    // if on other side of wall, reverse normal
+    // console.log(distance)
+    
+    // get approaching speed 
+    let vx = Math.abs(ob.dx)
+    let vy = Math.abs(ob.dy)
+    
+    // magnitude resulting force
+    let f = Math.abs(vx * nx + vy * ny)
+    
+    // split into parts and apply direction
+    let fx = f * nx * bounce
+    let fy = f * ny * bounce
 
-    ob.dx = -vx*bounce
-    ob.dy = -vy*bounce
+    // apply forces
+    ob.dx += fx*2
+    ob.dy += fy*2
     
-    let [dx, dy] = normalize_vector(vx, vy)
-    
-    ob.x += -dx*overlap
-    ob.y += -dy*overlap
+    // remove overlap
+    ob.x += nx*overlap
+    ob.y += ny*overlap
   }
 }
 
 function line_point_distance(ob, obs){
+  // line equation
   la = -(obs.y1 - obs.y0)/(obs.x1 - obs.x0)
   lb = -(obs.y0 + (la * obs.x0))
   
-  // right angle line
+  // normal vector
   let [dx, dy] = normalize_vector(-(obs.y0 - obs.y1), obs.x0 - obs.x1)
     
-  // right angle equation
+  // normal equation
   pa = -dy/dx
   pb = -(ob.y + (pa * ob.x))  
   
-  // get intersection point right angle
+  // calculate intersection point
   ix = (lb-pb)/(pa-la)
   iy = -(pa*ix+pb)
   
-  return Math.abs(Math.sqrt((ob.x - ix)*(ob.x - ix) + (ob.y - iy)*(ob.y - iy)))
+  // calcutale distance object and intersection point
+  return Math.sqrt((ob.x - ix)*(ob.x - ix) + (ob.y - iy)*(ob.y - iy))
 }
 
 function keyPressed() {
@@ -105,7 +118,7 @@ function keyPressed() {
 }
 
 function hit_obj(ob){
-  
+  // let every object collide with every other object once
   for(let i = ob.id; i < obj.length; i++){
     let ext_ob = obj[i]
     
@@ -115,26 +128,31 @@ function hit_obj(ob){
     if (distance > (ob.size/2)+(ext_ob.size/2)) continue
     let overlap = ob.size - distance
     
-    let [dx, dy] = normalize_vector((ob.x - ext_ob.x), (ob.y - ext_ob.y))
+    // normal vetor collision plane
+    let [nx, ny] = normalize_vector((ob.x - ext_ob.x), (ob.y - ext_ob.y))
     
+    // get approaching speed 
     let vx = Math.abs(ob.dx - ext_ob.dx)
     let vy = Math.abs(ob.dy - ext_ob.dy)
     
-    m = Math.abs(dx * vx + dy * vy) * bounce
+    // magitude resulting force
+    let f = Math.abs(vx * nx + vy * ny)
     
-    mx = m * dx
-    my = m * dy
+    // split into parts and apply direction
+    let fx = f * nx * bounce
+    let fy = f * ny * bounce
         
-    ob.dx += mx
-    ob.dy += my
+    // apply forces
+    ob.dx += fx
+    ob.dy += fy
+    ext_ob.dx -= fx
+    ext_ob.dy -= fy
     
-    ext_ob.dx -= mx
-    ext_ob.dy -= my
-    
-    ob.x += dx*overlap/2
-    ob.y += dy*overlap/2
-    ext_ob.x -= dx*overlap/2
-    ext_ob.y -= dy*overlap/2
+    // remove overlap
+    ob.x += nx*overlap/2
+    ob.y += ny*overlap/2
+    ext_ob.x -= nx*overlap/2
+    ext_ob.y -= ny*overlap/2
   }
 }
 
@@ -143,10 +161,13 @@ function draw_obj(ob){
   circle(ob.x, ob.y, ob.size)
 }
 
-function draw_obst(){
-  for(let o of obst){
-    line(o.x0, o.y0, o.x1, o.y1)
-  }
+function draw_obst(o){
+  line(o.x0, o.y0, o.x1, o.y1)
+  
+  let [nx, ny] = normalize_vector(-(o.y0 - o.y1), o.x0 - o.x1)
+  let cx = (o.x0 + o.x1)/2
+  let cy = (o.y0 + o.y1)/2
+  line(cx, cy, cx + nx*20, cy + ny*20)
 }
   
 function normalize_vector(dx, dy){
@@ -162,17 +183,18 @@ function apply_f(ob){
 
 
 function mouseClicked() {
-  obj.push(
-    {
-      id: obj.length,
-      size: 20,
-      x: mouseX,
-      y: mouseY,
-      dx: 0.0001,
-      dy: 0,
-      color: "#00A0FF"
-    }
-  )
+  for(let i = 0; i < 5; i++)
+    obj.push(
+      {
+        id: obj.length,
+        size: 25,
+        x: mouseX+25*i,
+        y: mouseY,
+        dx: 0.0001,
+        dy: 0,
+        color: "#00A0FF"
+      }
+    )
 }
 
 function setup() {
@@ -187,8 +209,10 @@ function draw() {
   for(let ob of obj){
     apply_f(ob)
     draw_obj(ob)
-    hit_obst(ob)
     hit_obj(ob)
+    hit_obst(ob)
   }
-  draw_obst()
+  for(let o of obst){
+    draw_obst(o)
+  }
 }
